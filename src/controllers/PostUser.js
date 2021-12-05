cc0183f1131e4f2580e87087c5d4423b4006ec3d
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-12-05 15:44:21 -0300

    feat: model que checa se email já existe

--- src/controllers/PostUser.js
+++ src/controllers/PostUser.js
@@ -1,8 +1,11 @@
 const { ValidateNewUser } = require('../validators');
 const { InvalidEntries } = require('../errors');
+const { User } = require('../models');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { error, value } = ValidateNewUser.validate(req.body);
   if (error) return res.status(422).json(InvalidEntries);
+  const EmailExists = await User.findOne({ email: req.body.email }).exec();
+  console.log(EmailExists);
   return res.status(200).send(value);
 };
--- src/models/index.js
+++ src/models/index.js
@@ -3,10 +3,23 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.Promise = global.Promise;
+
 const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
 
-async function main() {
-  await mongoose.connect(MONGO_DB_URL);
-}
+mongoose.connect(MONGO_DB_URL)
+  .then(() => console.log('Conectado ao MONGODB'))
+  .catch((err) => console.log(err));
 
-main().catch((err) => console.log(err));
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  role: String,
+});
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = {
+  User,
+};
